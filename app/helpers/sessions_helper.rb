c1b547431deba17ab4a1dcee38d9d2e83de1cf3b
Tatsuya <tatsuya.n.22128@gmail.com>
2019-11-11 06:45:58 +0000

    7章7.2

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,9 +11,17 @@ module SessionsHelper
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
+  
+  # 永続的セッションを破棄します。
+  def forget(user)
+    user.forget # Userモデル参照
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end 
 
   # セッションと@current_userを破棄します
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
